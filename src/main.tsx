8c40a4d138d5a2ef73dc72ecc461b390fc89bb15
Ashymov Aziret <aziretashym@gmail.com>
2024-11-13 16:58:06 +0600

    Created global storage and connected in "main.tsx". Also created "hooks.ts" for convenient using hooks.

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,15 @@ import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/*<Provider store={store}>*/}
+    <Provider store={store}>
       <BrowserRouter>
         <ToastContainer />
         <App />
       </BrowserRouter>
-    {/*</Provider>*/}
+    </Provider>
   </StrictMode>
 )
